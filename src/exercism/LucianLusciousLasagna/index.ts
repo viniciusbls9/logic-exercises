48bb7226e4f8619742fd9116aa465337c87348c8
Vinicius Benedito <beneditoloura@gmail.com>
2022-04-23 11:08:45 -0300

    🎉 initial commit

--- src/exercism/LucianLusciousLasagna/index.ts
+++ src/exercism/LucianLusciousLasagna/index.ts
@@ -4,7 +4,8 @@
  * The number of minutes it takes to prepare a single layer.
  */
 
-export const EXPECTED_MINUTES_IN_OVEN = 40;
+const EXPECTED_MINUTES_IN_OVEN = 40;
+const PREPARATION_MINUTES_PER_LAYER = 2
 
 /**
  * Determines the number of minutes the lasagna still needs to remain in the
@@ -13,7 +14,16 @@ export const EXPECTED_MINUTES_IN_OVEN = 40;
  * @param {number} actualMinutesInOven
  * @returns {number} the number of minutes remaining
  */
-export function remainingMinutesInOven(actualMinutesInOven: number) {
+export function remainingMinutesInOven(actualMinutesInOven: number): number {
   return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven
 }
 
+/**
+ * Given a number of layers, determines the total preparation time.
+ *
+ * @param {number} numberOfLayers
+ * @returns {number} the total preparation time
+ */
+export function preparationTimeInMinutes(numberOfLayers: number): number {
+  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER
+}
--- src/exercism/LucianLusciousLasagna/lasagna.spec.ts
+++ src/exercism/LucianLusciousLasagna/lasagna.spec.ts
@@ -1,7 +1,23 @@
-import { remainingMinutesInOven } from '.'
+import { remainingMinutesInOven, preparationTimeInMinutes } from '.'
+
+function totalTimeInMinutes(numberOfLayers: number, actualMinutesInOven: number): number {
+  const preparationLayersTime = numberOfLayers * 2
+  return preparationLayersTime + actualMinutesInOven
+}
+
 
 describe('LucianLusciousLasagna', () => {
   test('should remainingMinutesInOven returns how much time left', () => {
     expect(remainingMinutesInOven(30)).toBe(10)
+  })
+
+  test('should returns calculate the preparation time in minutes', () => {
+    expect(preparationTimeInMinutes(2)).toBe(4)
+  })
+
+  test('should calculates the total working time', () => {
+    const preparation = preparationTimeInMinutes(2)
+    const remain = remainingMinutesInOven(30)
+    expect(totalTimeInMinutes(preparation, remain)).toBe(34)
   })
 })
